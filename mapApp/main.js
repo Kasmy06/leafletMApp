d3357a20cc3b46199d02e41b83061e5ff23f8e39
Amani Samuel <88445877+Kasmy06@users.noreply.github.com>
2022-07-26 17:54:43 +0000

    Add files via upload

---
+++ mapApp/main.js
@@ -0,0 +1,8 @@
+
+        function ckeckUncheck(){
+            all = document.getElementsByName('acteurs');
+            for(var a=0;a<all.length;a++){
+                all[a].checked = main.checked;
+            }
+        }
+    
